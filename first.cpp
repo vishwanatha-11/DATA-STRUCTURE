6de84f90cbfe962ff58dc811e988ed8e32b2bc81
vishwanatha-11 <136451668+vishwanatha-11@users.noreply.github.com>
2023-08-19 21:18:08 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(){  //cout for print
+    cout<< "my name is VISHWA"<< endl; //endl for new line or \n
+}
---
+++ looop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int sum=0, n;
+int i=1;
+cin >> n;
+while (i<=n)
+{
+    //cout << i<< "  ";
+    sum=sum+i;
+    
+    i++;
+}
+cout <<sum;
+}
+
